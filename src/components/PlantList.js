e0be9e78f3b3afd45b58c15969a66d5fec62fca6
Austin Morse <amorse110@gmail.com>
2023-07-12 12:01:16 -0700

    first attempt

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList(props) {
+    
+  
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
-}
+      <PlantCard 
+        image={props.image}
+        name={props.name}
+        price={props.price}
+      />
+    )
+  }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  //const [plantList, setPlantList] = useState([])
+
+  // useEffect(() => {
+  //   fetch("http://localhost:6001/plants")
+  //     .then(res => res.json())
+  //     .then((data) => {setPlantList(data);});
+  // }, []);
+
   return (
     <main>
       <NewPlantForm />
