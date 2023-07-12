d9c51ad6b40a3ac5a184b5babd628e9591563809
Austin Morse <amorse110@gmail.com>
2023-07-12 14:33:15 -0700

    2nd Deliverable Complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -4,18 +4,22 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
-
   const [plants, setPlants] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:6001/plants")
       .then(res => res.json())
       .then(setPlants)
-  }, [])
+  }, []);
+
+  function addPlant(newPlant) {
+    console.log(newPlant)
+    setPlants(currPlants=> [...currPlants, newPlant]);
+  }
 
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={addPlant}/>
       <Search />
       <PlantList plants={plants}/>
     </main>
